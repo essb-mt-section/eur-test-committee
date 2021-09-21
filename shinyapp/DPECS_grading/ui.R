243841f935321ba8d2ef58718861e789f8d7894d
0liver Lindemann <lindemann09@googlemail.com>
2021-09-21 19:58:25 +0200

    minor

--- shinyapp/DPECS_grading/ui.R
+++ shinyapp/DPECS_grading/ui.R
@@ -7,27 +7,27 @@ shinyUI(fluidPage(
            </td><td align="right">
            <img src="./Logo-EUR-black.png" width="200" alt="Erasmus University">
            </td></tr></table>'),
-  
+
   sidebarLayout(
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+
       numericInput(inputId = "n_quest",
                    label = "Number of MC Questions:",
                    value = 40),
-      
+
       selectInput(inputId = "n_choices",
                   label = "Number of Choices:",
-                  choices=c(2,3,4,5), 
+                  choices=c(2,3,4,5),
                   selected = 4),
     ),
-    
+
     mainPanel(
-      uiOutput("subtitle"), 
+      uiOutput("subtitle"),
       tabsetPanel(type = "tabs",
                   tabPanel("Table", tableOutput("view")),
                   tabPanel("Graph/Formular", uiOutput("formula"), plotOutput("graph")),
-                  tabPanel("Explanation", uiOutput("explanation"), 
+                  tabPanel("Explanation", uiOutput("explanation"),
                            HTML("<p style='margin-top:50px;text-align:right'>
                            <i><small>(c) <a href='http://www.eur.nl/people/oliver-lindemann'>
                                 O. Lindemann</a></small></i></p>"))
@@ -37,4 +37,3 @@ shinyUI(fluidPage(
 ))
 
 
-  
